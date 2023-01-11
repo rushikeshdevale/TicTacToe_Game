b8340b664fc29061c2c16cd42573a86cee8e8ea4
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-11 10:46:48 +0530

    Taking input from user

--- src/com/game/TicTacToe.java
+++ src/com/game/TicTacToe.java
@@ -1,18 +1,38 @@
 package com.game;
+import java.util.Scanner;
 
 public class TicTacToe {
     private static char[] element;
+    static char userMark, computerMark;
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Tic Tac Toe Game !!!!!!");
-        boardCreation();
-    }
-
-    private static void boardCreation() {
-        element = new char[10];
-        for (int i = 1; i < 10; i++) {
-            element[i] = ' ';
+            System.out.println("Welcome to the Tic Tac Toe Game !!!!!!");
+            boardCreation();
+            choosingXorO();
         }
-        System.out.println("Empty board created");
-    }
+
+        private static void boardCreation() {
+            element = new char[10];
+            for (int i = 1; i < 10; i++) {
+                element[i] = ' ';
+            }
+        }
+        private static void choosingXorO() {
+            System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
+            int option = scan.nextInt();
+            switch (option) {
+                case 1:
+                    userMark = 'X';
+                    computerMark = 'O';
+                    break;
+                case 2:
+                    userMark = 'O';
+                    computerMark = 'X';
+                    break;
+                default:
+                    System.out.println("Your input is invalid");
+                   choosingXorO();
+            }
+        }
 }
